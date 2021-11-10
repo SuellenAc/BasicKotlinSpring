242d077a84972cecf1184e44df2067a24bbbaf8a
Suellen Colangelo <suellen.cola@gmail.com>
2021-11-10 11:43:28 -0300

    Adicionando método na classe TopicController.kt para buscar por um tópico por id. O método retorna null ou o tópico encontrado. Esse método precisa da anotação -  @GetMapping("/{id}") para indicar ao spring que a URI desse mapeamento aceita um path com o id do tópico a ser pesquisado. Para linkar o id do getMapping com o id do método utilizamos a anotação - PathVariable

--- src/main/kotlin/com/suellencolangelo/forum/controller/TopicController.kt
+++ src/main/kotlin/com/suellencolangelo/forum/controller/TopicController.kt
@@ -3,6 +3,7 @@ package com.suellencolangelo.forum.controller
 import com.suellencolangelo.forum.model.Topic
 import com.suellencolangelo.forum.service.TopicService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,5 +17,9 @@ class TopicController(private val service: TopicService) {
         return service.list()
     }
 
+    @GetMapping("/{id}")
+    fun searchById(@PathVariable id: Long): Topic? {
+        return service.searchById(id)
+    }
 
 }
--- src/main/kotlin/com/suellencolangelo/forum/service/TopicService.kt
+++ src/main/kotlin/com/suellencolangelo/forum/service/TopicService.kt
@@ -8,22 +8,28 @@ import org.springframework.stereotype.Service
 @Service
 class TopicService {
 
-    fun list(): List<Topic> = listOf(getDummyTopic(), getDummyTopic(), getDummyTopic())
-
-    private fun getDummyTopic(): Topic = Topic(
-        id = 1,
-        title = "Dúvida",
-        message = "Variaveis no kotlin",
-        course = Course(
-            id = 1,
-            name = "Kotlin",
-            category = "Programação",
-        ),
-        author = User(
-            id = 1,
-            name = "Ana da Silva",
-            email = "ana@email.com.br"
+    private val dummyTopic: Topic
+        get() = Topic(
+            id = (1L..10L).random(),
+            title = "Dúvida",
+            message = "Variaveis no kotlin",
+            course = Course(
+                id = (1L..10L).random(),
+                name = "Kotlin",
+                category = "Programação",
+            ),
+            author = User(
+                id = (1L..10L).random(),
+                name = "Ana da Silva",
+                email = "ana@email.com.br"
+            )
         )
-    )
 
+    private val topics: List<Topic> = listOf(dummyTopic, dummyTopic, dummyTopic)
+
+    fun list(): List<Topic> = topics
+
+    fun searchById(id: Long): Topic? {
+        return topics.firstOrNull { it.id == id }
+    }
 }
