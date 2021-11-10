af209827101d130468f4805151c94f3a8427884c
Suellen Colangelo <suellen.cola@gmail.com>
2021-11-09 22:08:28 -0300

    Adding spring initial project

---
+++ src/main/kotlin/com/suellencolangelo/forum/HelloController.kt
@@ -0,0 +1,13 @@
+package com.suellencolangelo.forum
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello") // endereço
+class HelloController {
+
+    @GetMapping // requisição do tipo get faz a chamada do metodo anotado com getMapping
+    fun hello(): String = "Hello World! Alterado"
+}
