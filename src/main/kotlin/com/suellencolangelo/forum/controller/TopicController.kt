e04beb61033f5158a6a9f8d55bda35e0fb08a1f3
Suellen Colangelo <suellen.cola@gmail.com>
2021-11-10 11:57:57 -0300

    Adicionando um metodo para buscar todas as respostas dado o id de um topico

--- src/main/kotlin/com/suellencolangelo/forum/controller/TopicController.kt
+++ src/main/kotlin/com/suellencolangelo/forum/controller/TopicController.kt
@@ -1,5 +1,6 @@
 package com.suellencolangelo.forum.controller
 
+import com.suellencolangelo.forum.model.Answer
 import com.suellencolangelo.forum.model.Topic
 import com.suellencolangelo.forum.service.TopicService
 import org.springframework.web.bind.annotation.GetMapping
@@ -20,6 +21,11 @@ class TopicController(private val service: TopicService) {
     @GetMapping("/{id}")
     fun searchById(@PathVariable id: Long): Topic? {
         return service.searchById(id)
+    }
+
+    @GetMapping("/{id}/answers")
+    fun searchByAnswerFromTopic(@PathVariable id: Long) : List<Answer> {
+        return  service.searchByAnswerFromTopic(id)
     }
 
 }
--- src/main/kotlin/com/suellencolangelo/forum/service/TopicService.kt
+++ src/main/kotlin/com/suellencolangelo/forum/service/TopicService.kt
@@ -1,5 +1,6 @@
 package com.suellencolangelo.forum.service
 
+import com.suellencolangelo.forum.model.Answer
 import com.suellencolangelo.forum.model.Course
 import com.suellencolangelo.forum.model.Topic
 import com.suellencolangelo.forum.model.User
@@ -9,27 +10,46 @@ import org.springframework.stereotype.Service
 class TopicService {
 
     private val dummyTopic: Topic
-        get() = Topic(
-            id = (1L..10L).random(),
-            title = "Dúvida",
-            message = "Variaveis no kotlin",
-            course = Course(
+        get() {
+            val topic = Topic(
                 id = (1L..10L).random(),
-                name = "Kotlin",
-                category = "Programação",
-            ),
-            author = User(
-                id = (1L..10L).random(),
-                name = "Ana da Silva",
-                email = "ana@email.com.br"
+                title = "Dúvida",
+                message = "Variaveis no kotlin",
+                course = Course(
+                    id = (1L..10L).random(),
+                    name = "Kotlin",
+                    category = "Programação",
+                ),
+                author = User(
+                    id = (1L..10L).random(),
+                    name = "Ana da Silva",
+                    email = "ana@email.com.br"
+                )
             )
-        )
+            return topic.copy(answers = listOf(makeDummyAnswer(topic)))
+        }
 
     private val topics: List<Topic> = listOf(dummyTopic, dummyTopic, dummyTopic)
+
+    private fun makeDummyAnswer(topic: Topic): Answer = Answer(
+        id = 1,
+        message = "Dúvida Respondida",
+        topic = topic,
+        author = User(
+            id = (1L..10L).random(),
+            name = "Mari Oliveira",
+            email = "mari@oliveira.com.br"
+        ),
+        isTopicSolution = false
+    )
 
     fun list(): List<Topic> = topics
 
     fun searchById(id: Long): Topic? {
         return topics.firstOrNull { it.id == id }
+    }
+
+    fun searchByAnswerFromTopic(id: Long): List<Answer> {
+        return topics.firstOrNull { it.id == id }?.answers.orEmpty()
     }
 }
